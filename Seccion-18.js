f5c811747cf81b0b325fa62ebfc3744d59e82156
Anderson <andersoneduardoop@ufps.edu.co>
2023-08-17 22:05:35 -0500

    first commit

---
+++ Seccion-18.js
@@ -0,0 +1,12 @@
+function getUsuarioActivo(nombre){
+    return{
+        uId: 'ABC567',
+        userName: nombre
+    }
+};
+
+
+const getUsuario = (nombre) => ({
+    uId:'ABC567',
+    userName: nombre
+})
